b9b51969eb5735f8b324ec44c49a82e2f31cdafb
Ramiro Spinelli <ramirospinelli92@gmail.com>
2023-12-30 18:19:17 -0300

    language picker emoji

--- src/partials/LanguagePicker.tsx
+++ src/partials/LanguagePicker.tsx
@@ -3,9 +3,12 @@ const LanguagePicker = () => {
   const language = window.location.pathname.replaceAll('/', '');
 
   return (
-    <div className="flex items-center justify-end">
-      {language === '' && <a href="/es/">Español</a>}
-      {language === 'es' && <a href="/">English</a>}
+    <div className="flex items-center gap-1">
+      🌐
+      <div className="flex items-center justify-end">
+        {language === '' && <a href="/es/">Español</a>}
+        {language === 'es' && <a href="/">English</a>}
+      </div>
     </div>
   );
 };
